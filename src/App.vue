dec3dfb7161fd2c0696cc30cd2bda9e5888367db
krystian-laubach-jr <krystian.laubach.jr@gmail.com>
2025-03-17 13:54:43 +0000

    remove default code ; add style ; add components with static data

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainHeader/>
+  <div class="container">
+    <UserBalance/>
+    <IncomeExpenses/>
+    <TransactionList/>
+    <AddTransaction/>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import MainHeader from './components/MainHeader.vue';
+import UserBalance from './components/UserBalance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
